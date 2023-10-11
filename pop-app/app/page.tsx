810921953ffecfc068f1c8e3974cc2b484dc2a33
Jonathan Bruenderman <jonbruenderman@hotmail.com>
2023-10-11 15:39:11 -0700

    uploading demo ready env

---
+++ pop-app/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import twitterPfp from 'public/assets/images/twitter-pfp-PoP-2.png';
+import winningIconMattIsGoingDown from 'public/assets/images/winningIcon.png';
+
+export default function Home() {
+  let background_cubes= {
+    backgroundImage: `url(/assets/images/Hero_Cubes_Only.png)`
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className='background_blocks' style={background_cubes}></div>
+      <Image className="hidden_not_so_hidden_image" src={winningIconMattIsGoingDown} alt="Winning Dealer"width={50} height={50}/>
+      <div className='title_pop'>
+        <Image src={twitterPfp}alt="Proof of Play Interview"width={50} height={50}/>
+        <div className='title_text'>Proof of Play Interview</div>
+      </div>
+    </main>
+  );
+}
